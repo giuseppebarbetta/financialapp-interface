d8f40b0f4f1c23c888e9122e2cf560ec4841ee17
giuseppebarbetta <giuseppebarbetta@outlook.com>
2024-07-21 20:58:31 -0300

    feat: init create filters

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,15 +1,25 @@
 import logo from '../../assets/logo.svg';
 import { Button } from '../../components/button/indesx';
-import { Header, Img } from './styles';
+import { Title } from '../../components/title';
+import { Filters, Header, Img, Main, Section } from './styles';
 
 export function Home() {
   return (
-    <Header>
-      <Img src={logo} />
-      <div>
-        <Button>Nova transação</Button>
-        <Button>Nova categoria</Button>
-      </div>
-    </Header>
+    <>
+      <Header>
+        <Img src={logo} />
+        <div>
+          <Button>Nova transação</Button>
+          <Button>Nova categoria</Button>
+        </div>
+      </Header>
+      <Main>
+        <Section>
+          <Filters>
+            <Title title="Saldo" subtitle="Receitas e despesas no período" />
+          </Filters>
+        </Section>
+      </Main>
+    </>
   );
 }
