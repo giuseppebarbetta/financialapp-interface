c3cf27db0bd1dfc5564a510135127adab852e5f1
giuseppebarbetta <giuseppebarbetta@outlook.com>
2024-07-23 20:35:25 -0300

    feat: create layout Charts and balances

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -3,9 +3,21 @@ import { InputMask } from '@react-input/mask';
 import logo from '../../assets/logo.svg';
 import { ButtonIcon } from '../../components/button-icon/indesx';
 import { Button } from '../../components/button/indesx';
+import { Card } from '../../components/card';
 import { Input } from '../../components/input';
 import { Title } from '../../components/title';
-import { Filters, Header, Img, InputDiv, Main, Section } from './styles';
+import {
+  Balance,
+  ChartAction,
+  ChartContainer,
+  ChartContent,
+  Filters,
+  Header,
+  Img,
+  InputDiv,
+  Main,
+  Section,
+} from './styles';
 
 export function Home() {
   return (
@@ -24,21 +36,58 @@ export function Home() {
             <InputDiv>
               <InputMask
                 component={Input}
-                mask="dd/mm/yyyy"
-                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                mask="dd/mm/aaaa"
+                replacement={{ d: /\d/, m: /\d/, a: /\d/ }}
                 label="Início"
-                placeholder="dd/mm/yyyy"
+                placeholder="dd/mm/aaaa"
               />
               <InputMask
                 component={Input}
-                mask="dd/mm/yyyy"
-                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                mask="dd/mm/aaaa"
+                replacement={{ d: /\d/, m: /\d/, a: /\d/ }}
                 label="Fim"
-                placeholder="dd/mm/yyyy"
+                placeholder="dd/mm/aaaa"
               />
               <ButtonIcon />
             </InputDiv>
           </Filters>
+
+          <Balance>
+            <Card title="Saldo" amount={1000000} />
+            <Card title="Saldo" amount={1000000} variant="incomes" />
+            <Card title="Saldo" amount={1000000} variant="expenses" />
+          </Balance>
+
+          <ChartContainer>
+            <header>
+              <Title
+                title="Gastos"
+                subtitle="Despesas por categoria no período"
+              />
+            </header>
+            <ChartContent></ChartContent>
+          </ChartContainer>
+
+          <ChartContainer>
+            <header>
+              <Title
+                title="Evolução Financeira"
+                subtitle="Saldo, Receitas e Gastos no ano"
+              />
+              <ChartAction>
+                <InputMask
+                  component={Input}
+                  mask="aaaa"
+                  replacement={{ a: /\d/ }}
+                  variant="black"
+                  label="Ano"
+                  placeholder="aaaa"
+                />
+                <ButtonIcon />
+              </ChartAction>
+            </header>
+            <ChartContent></ChartContent>
+          </ChartContainer>
         </Section>
       </Main>
     </>
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { theme } from '../../styles/theme';
+
 export const Header = styled.header`
   display: flex;
   align-items: center;
@@ -45,4 +47,37 @@ export const InputDiv = styled.div`
   max-width: 22.5rem;
   width: 100%;
   gap: 0.5rem;
+`;
+
+export const Balance = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  width: 100%;
+`;
+
+export const ChartContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  background-color: ${theme.colors.dark};
+  border-radius: 0.25rem;
+  gap: 0.75rem;
+
+  header {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+  }
+`;
+
+export const ChartAction = styled.div`
+  display: flex;
+  align-items: flex-end;
+  width: 8rem;
+  gap: 0.5rem;
+`;
+
+export const ChartContent = styled.div`
+  height: 14.5rem;
 `;
